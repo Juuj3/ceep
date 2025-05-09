a94a3787ec44a02fc0a433cb1f669b35fe3aa225
Juhhh_ <168776389+Juuj3@users.noreply.github.com>
2025-05-09 11:42:00 -0300

    alterações1

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySector ('.container');
+
+const numHexagons = 200;
+
+const colors = [' #007bff, #28a745, #ffc107'];
+
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function createHexagon() {
+    const hexagon = document.
+    createElement('div');
+    hexagon.classList.add('hexagon');
+
+    const x = getRandomNumber(0, window.innerWidth - 50);
+    const y = getRandomNumber(0, window.innerHeight)
+}
